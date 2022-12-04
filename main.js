e07213e5dbe33c761831fdbcdad3456057deee0e
andrewnsv <andrejnizovoj598@gmail.com>
2022-12-04 15:45:59 +0200

    Work without one task

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-//1
+//1 Объявите константу с целым числом числом и выведите ее в консоль в шестнадцатиричном и двочном виде.
 
 const num = 12321;
 
 console.log(num.toString(16)); 
 console.log(num.toString(2));
 
-//2
+//2 ВЫВОД В КОНСОЛЬ ОСУЩЕСТВИТЕ ПРИ ПОМОЩИ ШАБЛОННОЙ СТРОКИ, та что с `` кавычками и доларом ;)
 
 const myName = `Andrii`;
 const mysurname = `Nyzovyi`;
@@ -14,7 +14,7 @@ const fullName = `My name is ${mysurname} ${myName} ${myMiddleName}`;
 
 console.log(fullName);
 
-//3 
+//3 Используя prompt введите число как константу, затем выведите в консоль результаты операций + - * / на 2 над этим числом
 
 const enterTheNumber = prompt(`Введите любое число`);
 
@@ -23,18 +23,18 @@ console.log(`От вашего числа отняли 2 = ${+enterTheNumber - 2
 console.log(`Ваше число умножили на 2 = ${+enterTheNumber * 2}`);
 console.log(`Ваше число поделили на 2 = ${+enterTheNumber / 2}`);
 
-//4
+//4 Определити длинну строки введенной при помощи prompt и выведите длинну в консоль
 
 console.log(`Количество введеных символов = ${enterTheNumber.length}`);
 
-//5 
+//5 Округлите число 0.51000002 до двух знаков после запятой и прибавте к нему 2, результат выведете в консоль
 
-let nonInteger = 0.51000002;
+const nonInteger = 0.51000002;
 const roundedNumber = nonInteger.toFixed(2)
 
 console.log(`Округляем число ${+nonInteger} до двух знаков после запятой и добавляем к нему 2 = ${+roundedNumber + 2}`);
 
-//6 
+//6 Определите с какого символа строка str_two начинается в строке str. Преобразуте любую из этих строк к заглавным буквам, результат выведете в консольм
 
 const str = 'the quick brown fox jumps over the lazy dogs back';
 const str_two = 'brown fox jumps';
@@ -44,25 +44,21 @@ console.log(`Cтрока str_two начинается в строке str c ${fi
 
 console.log(str_two.toUpperCase());
 
-//7 
+//7 Сравните a c b, b c c, c c d и результаты между собой
 
 const a = 1;
 const b = 2; 
 const c = 3; 
 const d = 3;
 
-console.log(a || b || C);
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
+//8  Простой пример демонстрирующий различая между == и ===
 
+console.log('3' == 3); //true
+console.log('3' === 3); //false
 
+//9 Gродемонстрируйте работу оператора ??
 
+const first = null;
+const second = 2;
 
-
-
-
-
-
+console.log(first ?? second); //return 2 
